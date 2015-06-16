d7e38ca8b8207b91cb2311a9cf052f0f66902f68
GiovanneMomesso <giovanne.momesso@gmail.com>
2015-06-16 19:30:56 -0300

    teste2

---
+++ web/js/arquivo_javascript.js
@@ -0,0 +1,15 @@
+/* 
+ * Autor: Rafael Kozar
+ */
+function deletar_produto(){
+    var respost = confirm("Você tem certeza que deseja deletar este Produto?");
+    if(respost){
+        return 1;
+    }
+    else{
+        return 0;
+    }
+}
+
+        
+
